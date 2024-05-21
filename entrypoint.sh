84cd2f3e9fee95d600eb5086e7ce862c94a3a965
chkp-yossefk <152158101+chkp-yossefk@users.noreply.github.com>
2024-05-21 17:56:53 +0000

    fix supervisord

--- entrypoint.sh
+++ entrypoint.sh
@@ -178,7 +178,7 @@ if [[ ${_START_DOCKER_SERVICE} == "true" ]]; then
   _PREFIX=""
   [[ ${_RUN_AS_ROOT} != "true" ]] && _PREFIX="sudo"
   ## ilyaro: work arround issue with ulimit in docker 25 version https://github.com/docker/cli/issues/4807
-  sed 's#Hn#n#' /etc/init.d/docker > /etc/init.d/docker1; chmod +x /etc/init.d/docker1;mv /etc/init.d/docker1 /etc/init.d/docker
+  #sed 's#Hn#n#' /etc/init.d/docker > /etc/init.d/docker1; chmod +x /etc/init.d/docker1;mv /etc/init.d/docker1 /etc/init.d/docker
   ## set GUID for /tmp/work_dir, chmod +s /tmp/work_dir and /opt/hostedtoolcache both used in volumes dind
   chmod -R 777 ${_RUNNER_WORKDIR}
   chmod -R +s ${_RUNNER_WORKDIR}
@@ -192,7 +192,7 @@ fi
 
 if [[ ${_RUN_AS_ROOT} == "true" ]]; then
   if [[ $(id -u) -eq 0 ]]; then
-    ## ilyaro: to set permissions to 777 to enable job container non root user to save files on mounted volumes   
+    ## ilyaro: to set permissions to 777 to enable job container non root user to save files on mounted volumes
     umask 0000
     "$@"
   else
